26f940506321218b812f5689409fdff3dafec358
BORBECH <charlieborbs18@gmail.com>
2020-10-26 18:49:55 +0800

    Add files

---
+++ src/components/MultipleCounter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MultipleCounter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MultipleCounter;
