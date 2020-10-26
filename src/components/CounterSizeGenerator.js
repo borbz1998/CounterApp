3e3918d711170f45c0615215591e9618b155b73a
BORBECH <charlieborbs18@gmail.com>
2020-10-26 19:11:06 +0800

    [CounterApp][Charlie-Joseph] feature: Implement CounterGroup

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Counter from './Counter';
 
 class CounterSizeGenerator extends Component {
+
+
+
+
     render() {
         return (
             <div>
-                
+
             </div>
         );
     }
