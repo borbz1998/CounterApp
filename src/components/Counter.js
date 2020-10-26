beb187dff5f03966ca9c243bb65de6b354a053b6
BORBECH <charlieborbs18@gmail.com>
2020-10-26 18:02:11 +0800

    Initial Commit

---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+ 
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+ 
+        this.onIncrease = this.onIncrease.bind(this);
+        this.onDecrease = this.onDecrease.bind(this);
+ 
+ 
+        this.state = {
+            number: 0,
+        };
+    }
+ 
+    onIncrease() {
+        this.setState((prevState) => ({
+            number: prevState.number + 1
+        }))
+    }
+
+    onDecrease() {
+        this.setState((prevState) => ({
+            number: prevState.number - 1
+        }))
+    }
+ 
+    render() {
+        return (
+            <section>
+                <input type="button" value="+" onClick={(event) => {this.onIncrease()}} />
+                <span>{this.state.number}</span>
+                <input type="button" value="-" onClick={(event) => {this.onDecrease()}}/>
+            </section>
+        );
+    }
+}
+ 
+export default Counter;
---
+++ src/components/CounterGroup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CounterGroup extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CounterGroup;
