916ab8fd0bf34427838dd670c0da917d1a1d23e1
BORBECH <charlieborbs18@gmail.com>
2020-10-26 18:46:12 +0800

    Add new Files

---
+++ src/components/CounterGenerator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CounterGenerator extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CounterGenerator;
